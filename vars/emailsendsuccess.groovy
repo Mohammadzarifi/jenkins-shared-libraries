20eb2aacc72b174f753e95380a9e23caee5cfe07
Mohammadzarifi <iqbalzarifi@icloud.com>
2025-03-22 12:57:33 -0500

    checking from the beginning

--- vars/emailsendsuccess.groovy
+++ vars/emailsendsuccess.groovy
@@ -1,8 +1,8 @@
-def call() {
-    emailext(
-        attachLog: true, 
-        body: "Pipeline test: ${JOB_URL}", 
-        subject: "SUCCESSFUL JOB: Job name: ${JOB_NAME}", 
-        to: 'recipient@example.com' 
-    )
-}
+// def call() {
+//     emailext(
+//         attachLog: true, 
+//         body: "Pipeline test: ${JOB_URL}", 
+//         subject: "SUCCESSFUL JOB: Job name: ${JOB_NAME}", 
+//         to: 'recipient@example.com' 
+//     )
+// }
