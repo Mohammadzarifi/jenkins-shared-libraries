4d9502ca0d484edbb68a2b07f09edd001be5cf09
Mohammadzarifi <iqbalzarifi@icloud.com>
2025-03-22 11:46:45 -0500

    name fixed

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,3 +1,6 @@
-def call (String name) {
-    sh "echo ${name} this is jenkins shared library listing"
+def call(String name) {
+    if (!name?.trim()) {
+        error("Invalid input: Name cannot be null or empty.")
+    }
+    sh "echo ${name} this is Jenkins shared library listing"
 }
